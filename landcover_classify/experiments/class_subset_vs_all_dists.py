75bf59843fa5862b937d968c9fd4ff653778119b
7yl4r <git@tylar.info>
2020-01-28 15:07:16 -0500

    fn-ize img read to df

--- landcover_classify/experiments/class_subset_vs_all_dists.py
+++ landcover_classify/experiments/class_subset_vs_all_dists.py
@@ -15,46 +15,50 @@ from landcover_classify.read_bands_at \
 
 TRAIN_GLOB = 'data/GTPs_touse_points_*_train.shp'
 BAND_COLUMNS = ['band'+str(n) for n in range(8)]  # band0, band1, etc
-NTF_OR_RRS = "rrs"
 
 
-def main():
+def get_image_band_columns_at_point(src_file, points, fpath):
+    band_arry = read_bands_at(src_file, points, longformat=False)
+    class_df = pd.DataFrame(
+        band_arry,
+        columns=BAND_COLUMNS
+    )
+    class_df['x'] = [p[0] for p in points]
+    class_df['y'] = [p[1] for p in points]
+    cover_class = fpath.split("use_points_")[1].split("_t")[0]
+    class_df['cover_class'] = cover_class
+    class_df['src_file'] = src_file
+    return class_df
+
+
+def main(tif_files):
     df = pd.DataFrame(
         columns=[
             'x', 'y', 'cover_class', 'src_file'
         ] + BAND_COLUMNS
     )
-    for fpath in glob.glob(TRAIN_GLOB):
+    shp_files = glob.glob(TRAIN_GLOB)
+    print('inspecting {} .shp and {} .tif files'.format(
+        len(shp_files), len(tif_files)
+    ))
+    for fpath in shp_files:
         points = get_points_from_shapefile(fpath)
-        if NTF_OR_RRS == "ntf":
-            src_file = '16FEB12162517-M1BS-057380245010_01_P001.NTF'
-        elif NTF_OR_RRS == "rrs":
-            src_file = '16FEB12162517-M1BS-_RB_Rrs.tif'
-        else:
-            raise ValueError("need to set ntf or rrs")
-        band_arry = read_bands_at(src_file, points, longformat=False)
-        class_df = pd.DataFrame(
-            band_arry,
-            columns=BAND_COLUMNS
-        )
-        class_df['x'] = [p[0] for p in points]
-        class_df['y'] = [p[1] for p in points]
-        cover_class = fpath.split("use_points_")[1].split("_t")[0]
-        class_df['cover_class'] = cover_class
-        class_df['src_file'] = src_file
-        df = df.append(class_df)
+        for src_file in tif_files:
+            class_df = get_image_band_columns_at_point(src_file, points, fpath)
+            df = df.append(class_df)
 
-    df.to_csv("master_dataframe_{}.csv".format(NTF_OR_RRS))
+    df.to_csv("master_dataframe_rrs.csv")
 
     # === subset the results
     # exclude glint-free water
     pre_len = len(df)
     df = df.query('band7 >= 0.11')
-    print("{}/{} rows match subset ({:2.1f}%)".format(
-        len(df),
-        pre_len,
-        100*len(df)/pre_len
-    ))
+    if pre_len > 0:
+        print("{}/{} rows match subset ({:2.1f}%)".format(
+            len(df),
+            pre_len,
+            100*len(df)/pre_len
+        ))
 
     # === restructure cover_class into is_tgt_class true/false
     # ['Water', 'Mangrove']
@@ -64,7 +68,7 @@ def main():
     df[tgt_key].loc[df[tgt_key] == tgt_class] = 'yes'
     df[tgt_key].loc[df[tgt_key] != 'yes'] = 'no'
 
-    df.to_csv("{}_{}.csv".format(tgt_key, NTF_OR_RRS))
+    df.to_csv("{}_rrs.csv".format(tgt_key))
 
     # === restructure bands for violinplot
     # ref: https://stackoverflow.com/a/46134162/1483986
@@ -73,20 +77,25 @@ def main():
         id_vars=[tgt_key],
         value_vars=BAND_COLUMNS
     )
-    # import pdb; pdb.set_trace()
-
     # assert subset didn't cut out all tgt_class==yes or ==no
     assert len(pd.unique(df[tgt_key])) == 2
 
-    ax = sns.violinplot(
+    sns.violinplot(
         data=df, x='band_n', y='pixel_value',
         hue=tgt_key,
         split=True
     )
     plt.show()
-    plt.savefig(fname="subset_{}_bands_{}.png".format(
-        tgt_class.lower(), NTF_OR_RRS
+    plt.savefig(fname="subset_{}_bands_rrs.png".format(
+        tgt_class.lower()
     ))
 
+
 if __name__ == "__main__":
-    main()
+    image_files = glob.glob(
+        '16FEB12162517-M1BS-_RB_Rrs.tiff'
+        # 'images/tif_rrs_wv2/wv2_rrs_201803*.tif'
+        # 'wv2_Rrs_20161026T150644.tif'
+        # '16FEB12162517-M1BS-057380245010_01_P001.NTF'
+    )
+    main(image_files)
