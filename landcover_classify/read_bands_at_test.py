1515c6ff2adb3ab5d29eb6b4b39cd840f5f0f821
7yl4r <git@tylar.info>
2020-02-19 10:32:57 -0500

    + test for dataframe getter

--- landcover_classify/read_bands_at_test.py
+++ landcover_classify/read_bands_at_test.py
@@ -6,6 +6,7 @@ https://docs.python.org/3/library/unittest.html#assert-methods
 
 # std modules:
 from unittest import TestCase
+import numpy as np
 
 from landcover_classify.read_bands_at import read_bands_at
 
@@ -20,10 +21,34 @@ class Test_read_bands_at(TestCase):
             longformat=False
         )
         print(result)
-        self.assertEqual(
+        np.testing.assert_almost_equal(
             result,
             [[
-                0.08366379, 0.16067146, 0.2094495, 0.18627913,
-                0.17090823, 0.08640266, 0.04515372, 0.01725759
-            ]]
+                0.083664, 0.160671, 0.209449, 0.186279, 0.170908, 0.086403,
+                0.045154, 0.017258
+            ]],
+            decimal=5
+        )
+
+    def test_read_one_point_longformat(self):
+        """ Read one point from middle of the image """
+        result = read_bands_at(
+            '16FEB12162517-M1BS-_RB_Rrs.tiff',
+            [[-81.735270, 25.932147]],
+            longformat=True
+        )
+        print('*'*40)
+        print(result)
+        print('*'*40)
+        np.testing.assert_almost_equal(
+            result['band_n'].array,
+            [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
+        )
+        np.testing.assert_almost_equal(
+            result['pixel_value'].array,
+            [
+                    0.083664, 0.160671, 0.209449, 0.186279, 0.170908,
+                    0.086403, 0.045154, 0.017258
+            ],
+            decimal=5
         )
