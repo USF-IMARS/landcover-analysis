35968bba94b6c56e0defe882aa5e05c0f897b8ea
7yl4r <git@tylar.info>
2020-02-21 14:09:58 -0500

    append to df instead of new each time

--- landcover_classify/shapefile_points_to_master_rrs_csv.py
+++ landcover_classify/shapefile_points_to_master_rrs_csv.py
@@ -12,15 +12,19 @@ from landcover_classify.get_image_band_columns_at_point \
 
 SHAPEFILE_GLOB = 'data/GTPs_touse_points_*.shp'
 BAND_COLUMNS = ['band'+str(n) for n in range(8)]  # band0, band1, etc
+DATAFRAME_FILE = "master_dataframe_rrs.csv"
 
 
 def main(tif_files):
-    # for each shapefile
-    df = pd.DataFrame(
-        columns=[
-            'x', 'y', 'cover_class', 'src_file'
-        ] + BAND_COLUMNS
-    )
+    # open df to append to or create new
+    try:
+        df = pd.read_csv(DATAFRAME_FILE)
+    except FileNotFoundError:  # TODO catch file read error, not AssertionError
+        df = pd.DataFrame(
+            columns=[
+                'x', 'y', 'cover_class', 'src_file'
+            ] + BAND_COLUMNS
+        )
     shp_files = glob.glob(SHAPEFILE_GLOB)
     print('inspecting {} .shp and {} .tif files'.format(
         len(shp_files), len(tif_files)
@@ -31,7 +35,8 @@ def main(tif_files):
             class_df = get_image_band_columns_at_point(src_file, points, fpath)
             df = df.append(class_df)
 
-    df.to_csv("master_dataframe_rrs.csv")
+    df.drop_duplicates().reset_index(drop=True)
+    df.to_csv(DATAFRAME_FILE)
 
 
 if __name__ == "__main__":
